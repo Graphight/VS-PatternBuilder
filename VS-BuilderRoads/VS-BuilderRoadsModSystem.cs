50cecc211aee99de32e5f7e117001d483f39f803
Graphight <tom.ian.marsh@gmail.com>
2025-12-14 15:32:01 -0500

    load the official basic mod template to get started properly

---
+++ VS-BuilderRoads/VS-BuilderRoadsModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace VS_BuilderRoads;
+
+public class VS_BuilderRoadsModSystem : ModSystem
+{
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("vs-builderroads:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("vs-builderroads:hello"));
+    }
+}

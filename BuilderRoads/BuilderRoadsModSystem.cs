97b2beea240c1b172c88c818f36967ae005fd104
Graphight <tom.ian.marsh@gmail.com>
2025-12-14 15:51:49 -0500

    forgot a couple of references

--- BuilderRoads/BuilderRoadsModSystem.cs
+++ BuilderRoads/BuilderRoadsModSystem.cs
@@ -3,9 +3,9 @@ using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
 
-namespace VS_BuilderRoads;
+namespace BuilderRoads;
 
-public class VS_BuilderRoadsModSystem : ModSystem
+public class BuilderRoadsModSystem : ModSystem
 {
     // Called on server and client
     // Useful for registering block/entity classes on both sides
